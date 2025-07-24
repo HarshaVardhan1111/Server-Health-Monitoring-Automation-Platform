a243c2c293e30649d1aed14b59e3ce6899049923
HarshaVardhan1111 <harshajeya.11@gmail.com>
2025-07-25 03:25:59 +0530

    Phase 2 setup for docker-compose, prometheus, and updated main

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 from app.routers import jobs
 from app.db import init_db
 
-app = FastAPI(title="Server Health Monitoring & Automation API")
+app = FastAPI(title="Server Health Monitoring API")
 
 init_db()
 app.include_router(jobs.router)
 
+# Enable Prometheus metrics
+Instrumentator().instrument(app).expose(app)
+
 @app.get("/")
 def root():
-    return {"message": "Server Health Monitoring Platform is running."}
+    return {"message": "Server is healthy"}
